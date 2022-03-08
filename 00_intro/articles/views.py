62ca7d691d2f9c9f5e2ed23fc5cc40d54ae48eb6
winter-snow <winter.snow@kakaocorp.com>
2022-03-08 21:32:34 +0900

    Day 1: before url Mapping

---
+++ 00_intro/articles/views.py
@@ -0,0 +1,36 @@
+import random
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def variables(request):
+    foods = ['apple', 'banana', 'coconut',]
+    info = {
+        'name': 'Harry'
+    }
+    context = {
+        'info': info,
+        'foods': foods,
+    }
+    return render(request, 'variables.html', context)
+
+def filters(request):
+    foods = ['족발', '피자', '햄버거', '초밥',]
+    pick = random.choice(foods)
+    context = {
+        'pick': pick,
+        'foods': foods,
+    }
+    return render(request, 'filters.html', context)
+
+def throw(request):
+    return render(request, 'throw.html')
+
+def catch(request):
+    message = request.GET.get('message')
+    context = {
+        "message": message,
+    }
+    return render(request, 'catch.html', context)
