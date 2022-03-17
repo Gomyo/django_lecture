bfb23c085e89fc4ec89deab3565e0dfb6136cef6
gomyo <engcraft1102@gmail.com>
2022-03-18 00:03:29 +0900

    Django - 06 수정

--- 06_django_model_relationship/crud/articles/models.py
+++ 06_django_model_relationship/crud/articles/models.py
@@ -3,7 +3,6 @@ from django.conf import settings
 
 # Create your models here.
 class Article(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=10)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
